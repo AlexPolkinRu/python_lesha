d68b3e2a796fbb2a4704f15ac9e3f45f63d10f8c
Aleksandr Polochkin <alex.polkin@gmail.com>
2023-11-12 21:28:10 +0300

    Добавил exit_choice.py изменил if_else.py

---
+++ exit_choice.py
@@ -0,0 +1,10 @@
+exitChoice = "Nothing"
+while exitChoice != "EXIT":
+    print("1 - Начало игры")
+    print("2 - Настройки")
+    print("3 - Инструкция")
+    print("4 - Таблица рекордов")
+    print("EXIT - выход из игры")
+    # exitChoice = "EXIT"
+    exitChoice = input("Выберите пункт меню: ")
+print("Конец")
--- if_else.py
+++ if_else.py
@@ -1,5 +1,7 @@
 user_reply = input("Тебе нравятся роботы? (Напиши yes или no)")
 if user_reply == "yes":
     print("Beep boop!")
+elif user_reply == 'не знаю':
+    print("определись")
 else:
     print("Ну, роботам ты тоже не нравишься")
